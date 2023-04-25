e94e094925e7f4acde490c25668ccb21a039dbde
heider marquez <heidermadiaz@gmail.com>
2023-04-24 19:28:59 -0500

    avance de assets e imagen de avatar

--- lib/main.dart
+++ lib/main.dart
@@ -19,17 +19,53 @@ class NinjaCard extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/avaflu.png'),
+                  radius: 43.0,
+                ),
+              ),
               Text('Name',
                   style: TextStyle(
                     color: Colors.blueGrey,
                     letterSpacing: 2.0,
                   )),
+              SizedBox(height: 10.0),
               Text('Chun Li',
                   style: TextStyle(
                       color: Colors.amberAccent[200],
                       letterSpacing: 2.0,
                       fontSize: 28.8,
                       fontWeight: FontWeight.bold)),
+              SizedBox(height: 30.0),
+              Text('Edad',
+                  style: TextStyle(
+                    color: Colors.blueGrey,
+                    letterSpacing: 2.0,
+                  )),
+              SizedBox(height: 10.0),
+              Text('28',
+                  style: TextStyle(
+                      color: Colors.amberAccent[200],
+                      letterSpacing: 2.0,
+                      fontSize: 28.8,
+                      fontWeight: FontWeight.bold)),
+              SizedBox(height: 30.0),
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[400],
+                  ),
+                  SizedBox(width: 10.0),
+                  Text('prueba@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontSize: 18.0,
+                        letterSpacing: 2.0,
+                      )),
+                ],
+              )
             ],
           ),
         ));
